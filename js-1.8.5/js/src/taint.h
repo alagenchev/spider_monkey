3c85d6cbbcfe7ce0fcd29a218b23164f68ff6452
alagenchev <alagenchev@gmail.com>
2013-03-12 17:03:52 -0400

    added structs needed for tainting

--- js-1.8.5/js/src/taint.h
+++ js-1.8.5/js/src/taint.h
@@ -3,9 +3,45 @@
 #define TAINT_H_
 
 #ifdef TAINT_ON_
+
+
+#ifndef TAINTSTRUCTS
+#define TAINTSTRUCTS
+//remember this is duplicated in jscntxt.h
+//// any change here must be replicated there
+//// also OpNames must be added in taint.cpp
+typedef enum taintop {NONEOP,GET,SET,SOURCE,SINK,CHARAT,SUBSTRING,LOWERCASE,UPPERCASE,JOIN,SPLIT,SLICE,REPLACE,REGEXP,CONCAT,CONCATLEFT,CONCATRIGHT,ESCAPE,UNESCAPE,ENCODEURI,UNENCODEURI,ENCODEURICOMPONENT,UNENCODEURICOMPONENT,TRIM,TAGIFY,QUOTE,DEPEND,ATOB,BTOA} TaintOp;
+
+
+typedef struct InfoTaintEntry
+{
+    JSString *taintedString;
+    jsuint refCount;
+    TaintOp  op;
+    JSString *origin;
+    struct InfoTaintDep *dep;
+    struct InfoTaintEntry *nextEntry;
+} InfoTaintEntry;
+
+
+typedef struct InfoTaintDep
+{
+    InfoTaintEntry *entry;
+    int spos;
+    int epos;
+    JSObject *desc;
+    struct InfoTaintDep *next;
+} InfoTaintDep;
+
+#endif
+
 //macros to hook up javascript to cpp
 //it's used in jsstr.cpp
-#define ADD_JS_TAINT_METHODS()\
+#define HOOK_JS_TAINT_METHODS()\
         JS_FN("newTainted", str_newTainted, 2, 0),
 #endif //endif for TAINTED_ON
+
+
+extern JSBool taint_newTainted(JSContext *cx, uintN argc, jsval *vp);
+extern JSBool InitTaintEntries(JSRuntime *rt);
 #endif
