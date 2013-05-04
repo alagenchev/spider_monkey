993e95f81f07aa091b4a21e142b27f104bd507ae
alagenchev <alagenchev@gmail.com>
2013-05-04 18:05:55 -0400

    adding simple taint info test

--- js-1.8.5/js/src/tainttests/infoTest.js
+++ js-1.8.5/js/src/tainttests/infoTest.js
@@ -1,7 +1,8 @@
 load('./UnitTest.js');
 var taintedString = String.newTainted("myTaintedStringValue", "myTaintedSource");
 var untainted = "";
-//assert("\ntainted assert:", taintedString.tainted, true);
-//assert("\nuntainted assert:", untainted.tainted, false);
 var taintInfo = String.getTaintInfo(taintedString);
-print(getObjectDetails(taintInfo, 1));;
+result = getObjectDetails(taintInfo);
+
+var expected = "val: \"myTaintedStringValue\" origin: \"myTaintedSource\" op: \"SOURCE\" ";
+assert("infoTest: ", result == expected, true);
