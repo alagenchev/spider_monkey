664149895e9fa4a3c768ac62a4f0109ec3b0a115
alagenchev <alagenchev@gmail.com>
2013-05-04 17:58:59 -0400

    adding more concatinfo tests

--- js-1.8.5/js/src/tainttests/concatInfoTest.js
+++ js-1.8.5/js/src/tainttests/concatInfoTest.js
@@ -6,41 +6,93 @@ var zerolengthuntainted="";
 var longTainted = String.newTainted("aaaaaaaaaaaaaaaaaaaaaaaa", "asdf");
 var longUntainted = "aaaaaaaaaaaaaaaaaaaaaaaa"
 
+var result = "";
+var expected = "";
+
 var concstr1=ts+ats;
-assert( "String Concat test ","concstr1.tainted",true)
-
 var infoResult = String.getTaintInfo(concstr1);
+var expected = "val: \"tsStringatsString\" op: \"CONCAT\" \n"+ 
+"tainters: \"{ \n"+ 
+        "\t0: \"{ StartPosition: \"8\" EndPosition: \"17\" val: \"atsString\" origin: \"aaa\" op: \"SOURCE\" } \n\t"+ 
+                "1: \"{ StartPosition: \"0\" EndPosition: \"8\" val: \"tsString\" origin: \"dd\" op: \"SOURCE\" } }\n";
 
-print(getObjectDetails(infoResult));
 
-/*
+var result = getObjectDetails(infoResult);
+assert("concstr1: ", result == expected, true);
+
+
 var concstr2=ts+ats+'dddd';
-assert( "String Concat test ","concstr2.tainted",true)
+var infoResult = String.getTaintInfo(concstr2);
+result = getObjectDetails(infoResult);
+
+expected = "val: \"tsStringatsStringdddd\" op: \"CONCATLEFT\" \n"+ 
+"tainters: \"{ \n" +
+"\t0: \"{ StartPosition: \"0\" EndPosition: \"17\" val: \"tsStringatsString\" op: \"CONCAT\" \n"+
+"tainters: \"{ \n"+ 
+"\t0: \"{ StartPosition: \"8\" EndPosition: \"17\" val: \"atsString\" origin: \"aaa\" op: \"SOURCE\" } \n"+
+"\t1: \"{ StartPosition: \"0\" EndPosition: \"8\" val: \"tsString\" origin: \"dd\" op: \"SOURCE\" } }\n} }\n";
+
+assert("concstr2: ", result == expected, true);
+
 
 var concstr3='dddd'+ts+ats;
-assert("String Concat test ","concstr3.tainted",true)
+var infoResult = String.getTaintInfo(concstr3);
+result = getObjectDetails(infoResult);
+
+expected = "val: \"ddddtsStringatsString\" op: \"CONCAT\" \n"+
+"tainters: \"{ \n\t"+ 
+"0: \"{ StartPosition: \"12\" EndPosition: \"21\" val: \"atsString\" origin: \"aaa\" op: \"SOURCE\" } \n\t"+ 
+"1: \"{ StartPosition: \"0\" EndPosition: \"12\" val: \"ddddtsString\" op: \"CONCATRIGHT\" \n"+ 
+"tainters: \"{ \n\t" +
+"0: \"{ StartPosition: \"4\" EndPosition: \"12\" val: \"tsString\" origin: \"dd\" op: \"SOURCE\" } }\n} }\n";
+
+assert("concstr3: ", result == expected, true);
 
 var concstr4=ts+'dddd'+ats;
-assert("String Concat test ","concstr4.tainted",true)
-assert("testing values ","concstr4","ddddd\ndddddddaaagggg");
+var infoResult = String.getTaintInfo(concstr4);
+result = getObjectDetails(infoResult);
 
-var concstr5zl=zerolengthtainted+''+zerolengthuntainted;
-assert("String Concat test ","concstr5zl.tainted",true)
-assert("testing values ","concstr5zl","");
+expected = "val: \"tsStringddddatsString\" op: \"CONCAT\" \n"+ 
+"tainters: \"{ \n\t"+ 
+"0: \"{ StartPosition: \"12\" EndPosition: \"21\" val: \"atsString\" origin: \"aaa\" op: \"SOURCE\" } \n\t"+ 
+"1: \"{ StartPosition: \"0\" EndPosition: \"12\" val: \"tsStringdddd\" op: \"CONCATLEFT\" \n"+ 
+"tainters: \"{ \n\t"+ 
+"0: \"{ StartPosition: \"0\" EndPosition: \"8\" val: \"tsString\" origin: \"dd\" op: \"SOURCE\" } }\n} }\n";
 
-var test7string = longUntainted + zerolengthuntainted;
-assert("Long String concat test7", "test7string.tainted", false);
-assert("testing values ","test7string","aaaaaaaaaaaaaaaaaaaaaaaa");
+assert("concstr4: ", result == expected, true);
 
-var test6string = longTainted + zerolengthuntainted;
-assert("Long String concat test1", "test6string.tainted", true);
-assert("testing values ","test6string","aaaaaaaaaaaaaaaaaaaaaaaa");
+var concstr5=zerolengthtainted+''+zerolengthuntainted;
+var infoResult = String.getTaintInfo(concstr5);
+result = getObjectDetails(infoResult);
 
-var test8string = longUntainted + zerolengthtainted;
-assert("Long String concat test8", "test8string.tainted", true);
-assert("testing values ","test8string","aaaaaaaaaaaaaaaaaaaaaaaa");
+expected = "val: \"\" op: \"CONCATLEFT\" \n"+ 
+"tainters: \"{ \n\t"+ 
+"0: \"{ StartPosition: \"0\" EndPosition: \"0\" val: \"\" op: \"CONCATLEFT\" \n"+ 
+"tainters: \"{ \n\t"+ 
+"0: \"{ StartPosition: \"0\" EndPosition: \"0\" val: \"\" origin: \"zerolength\" op: \"SOURCE\" } }\n} }\n";
 
-var info = String.getTaintInfo(test8string);
-var description = getObjectDetails(info);
-print(description);
-*/
+assert("concstr5: ", result == expected, true);
+
+var concstr6 = longTainted + zerolengthuntainted;
+var infoResult = String.getTaintInfo(concstr6);
+result = getObjectDetails(infoResult);
+
+expected = "val: \"aaaaaaaaaaaaaaaaaaaaaaaa\" op: \"CONCATLEFT\" \n"+ 
+"tainters: \"{ \n\t"+
+"0: \"{ StartPosition: \"0\" EndPosition: \"24\" val: \"aaaaaaaaaaaaaaaaaaaaaaaa\" origin: \"asdf\" op: \"SOURCE\" } }\n";
+
+assert("concstr6: ", result == expected, true);
+
+var concstr7 = longUntainted + zerolengthuntainted;
+var infoResult = String.getTaintInfo(concstr7);
+result = getObjectDetails(infoResult);
+expected = "";
+assert("concstr7: ", result == expected, true);
+
+var concstr8 = longUntainted + zerolengthtainted;
+var infoResult = String.getTaintInfo(concstr8);
+result = getObjectDetails(infoResult);
+expected = "val: \"aaaaaaaaaaaaaaaaaaaaaaaa\" op: \"CONCATRIGHT\" \n"+ 
+"tainters: \"{ \n\t"+ 
+"0: \"{ StartPosition: \"24\" EndPosition: \"24\" val: \"\" origin: \"zerolength\" op: \"SOURCE\" } }\n";
+assert("concstr8: ", result == expected, true);
