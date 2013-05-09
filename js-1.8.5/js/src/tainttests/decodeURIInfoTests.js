53a00aaf361124a69333860ef1473b9fcd9daa95
alagenchev <alagenchev@gmail.com>
2013-05-08 22:17:41 -0400

    adding decodeURIInfo tests

---
+++ js-1.8.5/js/src/tainttests/decodeURIInfoTests.js
@@ -0,0 +1,40 @@
+load('./UnitTest.js');
+var zerolengthtainted=String.newTainted("","zerolength");
+var ts=String.newTainted("dddddddd","dd");
+var ats=String.newTainted("aaagggg","ddd");
+var zerolengthuntainted="";
+var longTainted = String.newTainted("aaaaaaaaaaaaaaaaaaaaaaaa", "asdf");
+var longUntainted = "aaaaaaaaaaaaaaaaaaaaaaaa"
+
+
+var fun= decodeURI
+var ets=  fun(ts);
+
+var infoResult = String.getTaintInfo(ets);
+var expected = "val: \"dddddddd\" op: \"DECODEURI\" \n"+
+"tainters: \"{ \n\t0: \"{ StartPosition: \"-1\" EndPosition: \"-1\" val: \"dddddddd\""+ " origin: \"dd\" op: \"SOURCE\" } }\n";
+
+var result = getObjectDetails(infoResult);
+assert("ets: ", result == expected, true);
+
+
+var ets1= fun(zerolengthtainted );
+
+
+expected = "val: \"\" op: \"DECODEURI\" \n"+
+"tainters: \"{ \n\t"+
+"0: \"{ StartPosition: \"-1\" EndPosition: \"-1\" val: \"\" origin: \"zerolength\" op: \"SOURCE\" } }\n";
+
+infoResult = String.getTaintInfo(ets1);
+result = getObjectDetails(infoResult);
+assert("ets1 : ", result == expected, true);
+
+var ets2= fun(zerolengthuntainted );
+
+expected = "";
+
+infoResult = String.getTaintInfo(ets2);
+result = getObjectDetails(infoResult);
+
+assert("ets2 : ", result == expected, true);
+
