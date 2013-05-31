3ad6c7a057fcfd7042bf3943c0ccdb21c90989a3
Ivan Alagenchev <alagenchev@gmail.com>
2013-05-31 15:29:14 -0700

    fixing a minor issue with splitTest causing the test to give a undefined type error

--- js-1.8.5/js/src/tainttests/splitTest.js
+++ js-1.8.5/js/src/tainttests/splitTest.js
@@ -1,6 +1,6 @@
 load('./UnitTest.js');
 var zerolengthtainted=String.newTainted("","zerolength");
-var ts=String.newTainted("tsString","dd");
+var ts=String.newTainted("tsString\ntest","dd");
 var ats=String.newTainted("atsString","aaa");
 var zerolengthuntainted="";
 var longTainted = String.newTainted("aaaaaaaaaaaaaaaaaaaaaaaa", "asdf");
