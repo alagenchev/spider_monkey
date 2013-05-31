c3d0e40550e6faced30f005fc5e48d75034457b1
Ivan Alagenchev <alagenchev@gmail.com>
2013-05-31 14:52:51 -0700

    adding split test

---
+++ js-1.8.5/js/src/tainttests/splitTest.js
@@ -0,0 +1,16 @@
+load('./UnitTest.js');
+var zerolengthtainted=String.newTainted("","zerolength");
+var ts=String.newTainted("tsString","dd");
+var ats=String.newTainted("atsString","aaa");
+var zerolengthuntainted="";
+var longTainted = String.newTainted("aaaaaaaaaaaaaaaaaaaaaaaa", "asdf");
+var longUntainted = "aaaaaaaaaaaaaaaaaaaaaaaa"
+
+var splitar = ts.split("\n");
+//assert("String split test ","splitar[0].tainted",true);
+assert("String split test ","splitar[1].tainted",true);
+/*
+var tssplit=String.newTainted("\n","dd").split('\n');
+assert("String split test ","tssplit[0].tainted",true);
+assert("String split test ","tssplit[1].tainted",true);
+*/
