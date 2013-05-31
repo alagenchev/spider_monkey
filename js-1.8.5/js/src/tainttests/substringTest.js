2a87665e0d11f56a88ecb7f6eea090f86bffc8b5
Ivan Alagenchev <alagenchev@gmail.com>
2013-05-31 10:12:07 -0700

    adding test for substring op

---
+++ js-1.8.5/js/src/tainttests/substringTest.js
@@ -0,0 +1,19 @@
+load('./UnitTest.js');
+var zerolengthtainted=String.newTainted("","zerolength");
+var ts=String.newTainted("tsString","dd");
+var ats=String.newTainted("atsString","aaa");
+var zerolengthuntainted="";
+var longTainted = String.newTainted("aaaaaaaaaaaaaaaaaaaaaaaa", "asdf");
+var longUntainted = "aaaaaaaaaaaaaaaaaaaaaaaa"
+
+var ets=  ts.substring(1,3);
+
+assert("String substring test ","ets.tainted",true)
+
+/*
+var ets1= zerolengthtainted.substring(1,3) ;
+assert("String substring test ","ets1.tainted",true);
+
+var ets2=  zerolengthuntainted.substring(1,3) ;
+assert("String substring test ","ets2.tainted",false)
+*/
