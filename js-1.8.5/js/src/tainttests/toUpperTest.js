25f091ea41d8867cf4fb62f0e1a25e426ab4edfd
Ivan Alagenchev <alagenchev@gmail.com>
2013-05-31 08:45:43 -0700

    adding toUpper test

---
+++ js-1.8.5/js/src/tainttests/toUpperTest.js
@@ -0,0 +1,19 @@
+load('./UnitTest.js');
+var zerolengthtainted=String.newTainted("","zerolength");
+var ts=String.newTainted("tsString","dd");
+var ats=String.newTainted("atsString","aaa");
+var zerolengthuntainted="";
+var longTainted = String.newTainted("aaaaaaaaaaaaaaaaaaaaaaaa", "asdf");
+var longUntainted = "aaaaaaaaaaaaaaaaaaaaaaaa"
+
+var fun=  String.toUpperCase
+var ets=  fun(ts);
+assert("String toUpperCase test ","ets.tainted",true)
+
+/*
+var ets1= fun(zerolengthtainted );
+assert("String toUpperCase test ","ets1.tainted",true);
+
+var ets2= fun(zerolengthuntainted );
+assert("String toUpperCase test ","ets2.tainted",false)
+*/
